0d0d44722ecbecdb3e63398aab4fc1158dd12260
ktum787​🔹 <150287777+KanTum99@users.noreply.github.com>
2026-04-05 08:42:11 +0700

    Add color codes and log functions for the beby CLI tool

---
+++ lib/utils.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Color codes
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+BLUE="\033[0;34m"
+MAGENTA="\033[0;35m"
+CYAN="\033[0;36m"
+RESET="\033[0m"
+
+# Log functions
+log_info() {
+    echo -e "${GREEN}[INFO]${RESET} $1"
+}
+
+log_warning() {
+    echo -e "${YELLOW}[WARNING]${RESET} $1"
+}
+
+log_error() {
+    echo -e "${RED}[ERROR]${RESET} $1"
+}
+
+log_debug() {
+    echo -e "${BLUE}[DEBUG]${RESET} $1"
+}
